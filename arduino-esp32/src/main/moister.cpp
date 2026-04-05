64caf337880736fd503ff2e72671672548c3987d
koeir <kianjohnban@gmail.com>
2026-04-05 21:09:10 +0800

    log: integrated ph4502c module

--- arduino-esp32/src/main/moister.cpp
+++ arduino-esp32/src/main/moister.cpp
@@ -10,19 +10,17 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <cstdint>
+#include <ph4502c_sensor.h>
 
 #define BAUDRATE 115200
 
 // BLE -----------------------------------
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
 #define SERVICE_UUID        "23a6cc2f-19ce-4108-a792-65d17bc0c958"
 #define CHARACTERISTIC_UUID "4ba5b87d-bcd6-4e13-beb1-b52c1fcc604f"
 // --------------------------------------
 
 // Moisture sensor ----
-#define SIG GPIO_NUM_36
+#define MOIST_SIG GPIO_NUM_36
 uint16_t readings_cache[CACHE_MAX];
 uint16_t average = 0;
 uint8_t  package[sizeof(uint16_t) * 2];
@@ -31,18 +29,48 @@ uint8_t  cache_index = 0;
 uint32_t sum         = 0;
 // --------------------
 
+// pH + temp sensor ----
+/* Pinout: https://cdn.awsli.com.br/969/969921/arquivos/ph-sensor-ph-4502c.pdf */
+#define TEMPERATURE_PIN 34
+#define PH_PIN 35
+#define PH_TRIGGER_PIN 14
+#define CALIBRATION 14.8f
+#define READING_INTERVAL 100
+#define READING_COUNT 10
+// NOTE: The ESP32 ADC has a 12-bit resolution (while most arduinos have 10-bit)
+#define ADC_RESOLUTION 4096.0f
+
+// Create an instance of the Sensor
+PH4502C_Sensor pH_sensor_module(
+  PH_PIN,
+  TEMPERATURE_PIN,
+  CALIBRATION,
+  READING_INTERVAL,
+  READING_COUNT,
+  ADC_RESOLUTION
+);
+
+int temperature = 0;
+int pH_level_single = 0;
+int pH_level_average = 0;
+// ---------------------
+
 void setup() {
     Serial.begin(BAUDRATE);
 
     setup_ble_notify("ESP32", SERVICE_UUID, CHARACTERISTIC_UUID);
+    pH_sensor_module.init();
 
     analogSetAttenuation(ADC_11db); // ~3.3V input
     Serial.println("Waiting a client connection to notify...");
 
     // Headers for the moisture readings lol
-    Serial.println("_________________________________");
-    Serial.println("|   average    |      current   |");
-    Serial.println("---------------------------------");
+    Serial.println(
+"_______________________________________________________________\n"
+"|        MOIST        |         pH         |       TEMP        |\n"
+"---------------------------------------------------------------\n"
+"| avg     | current   | avg     | current  |      value        |\n"
+"---------------------------------------------------------------");
 }
 
 void loop() {
@@ -50,19 +78,12 @@ void loop() {
     cache_index = INDEX_WRAP(count, CACHE_MAX);
 
     // Read moisture sensor
-    readings_cache[cache_index] = analogRead(SIG);
+    readings_cache[cache_index] = analogRead(MOIST_SIG);
 
     sum += readings_cache[cache_index];
 
     // If certain number of readings have been made
     if (count == CACHE_MAX) {
-
-        // Headers for the moisture readings lol
-        Serial.print("\033[3A");
-        Serial.println("_________________________________");
-        Serial.println("|   average    |      current   |");
-        Serial.println("---------------------------------");
-
         // Get average of last 20 reads
         average = sum / count;
 
@@ -71,12 +92,22 @@ void loop() {
         sum   = 0;
     }
 
+    temperature = pH_sensor_module.read_temp();
+    pH_level_single = pH_sensor_module.read_ph_level_single();
+    pH_level_average = pH_sensor_module.read_ph_level();
+
     // Put readings in byte array in little-endian
     u16_bytepack(package, sizeof(package), 0, average);
     u16_bytepack(package, sizeof(package), 1, readings_cache[cache_index]);
 
-    Serial.printf("|     %i       |     %i      |\n", average,
-                  readings_cache[cache_index]);
+    Serial.printf(
+"| %7d | %9d | %7d | %9d | %15d |\n",
+average,
+readings_cache[cache_index],
+pH_level_average,
+pH_level_single,
+temperature
+);
     Serial.print("\033[1A");
 
     if (deviceConnected) {
